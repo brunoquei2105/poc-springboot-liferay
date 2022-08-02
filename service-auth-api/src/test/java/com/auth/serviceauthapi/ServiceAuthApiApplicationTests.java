09c78f0bd543d4956265010ba346f7c6455096f4
Bruno Queiroz <bruno.queiroz@liferay.com>
2022-08-02 09:35:06 -0300

    Service auth api

---
+++ service-auth-api/src/test/java/com/auth/serviceauthapi/ServiceAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.serviceauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
