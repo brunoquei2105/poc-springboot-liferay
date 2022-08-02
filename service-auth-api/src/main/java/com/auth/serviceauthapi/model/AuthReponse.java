b7e85cd43a0d3a155d427cf97952c87d336d4033
Bruno Queiroz <bruno.queiroz@liferay.com>
2022-08-02 14:01:17 -0300

    Implementing Auth Service.

---
+++ service-auth-api/src/main/java/com/auth/serviceauthapi/model/AuthReponse.java
@@ -0,0 +1,21 @@
+package com.auth.serviceauthapi.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class AuthReponse implements Serializable {
+
+    private static final long serialVersionUID = 5469607262231137281L;
+
+    private String userName;
+    private String token;
+
+    @JsonProperty("expires_in")
+    private int expiresIn;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+}
